a366bfb1b39d6b66ea5049cedd0ae150745788b5
Azurtilo <joseacunapuente14@gmail.com>
2024-10-08 16:27:55 -0300

    creacion clase cliente y producto y conexion firebase

---
+++ app/src/main/java/cl/acuna/ventassimplesenvivo/model/cliente.java
@@ -0,0 +1,59 @@
+package cl.acuna.ventassimplesenvivo.model;
+
+public class cliente {
+
+    public String uid;
+    public String Nombre;
+    public String Apellido;
+    public String Telefono;
+    public String Direccion;
+
+    //contructor
+    public cliente() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        Telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return Nombre;
+    }
+}
