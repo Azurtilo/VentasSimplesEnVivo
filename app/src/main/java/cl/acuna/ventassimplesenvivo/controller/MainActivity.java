3e8f5aca1db0b8cd8b656f71cb69923f8168727e
Azurtilo <joseacunapuente14@gmail.com>
2024-10-08 18:10:19 -0300

    prueba formulario y menu cliente

--- app/src/main/java/cl/acuna/ventassimplesenvivo/controller/MainActivity.java
+++ app/src/main/java/cl/acuna/ventassimplesenvivo/controller/MainActivity.java
@@ -1,12 +1,17 @@
-package cl.acuna.ventassimplesenvivo;
+package cl.acuna.ventassimplesenvivo.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import cl.acuna.ventassimplesenvivo.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button botonMenuCliente = (Button)findViewById(R.id.boton_menu_cliente);
+
+        botonMenuCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, menu_cliente.class));
+            }
         });
     }
 }
